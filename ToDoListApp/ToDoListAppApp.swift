083680b808b4f4e86943cbb48f82b314185cd69a
realeti <oro.eto.bristol@gmail.com>
2025-03-05 20:00:42 +0200

    First commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by realeti on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
